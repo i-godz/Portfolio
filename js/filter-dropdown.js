b5fd7209b67d78878207c819ce2c1611714ca030
unknown <zyadwael366@gmail.com>
2023-08-01 02:26:33 +0300

    commit

--- js/filter-dropdown.js
+++ js/filter-dropdown.js
@@ -23,7 +23,7 @@
       if (checked && checked.length > 0) {
         btnText.innerText = `${checked.length} Selected`;
       } else {
-        btnText.innerText = "Filter Tech";
+        btnText.innerText = "Tech Filter";
       }
     }
 
